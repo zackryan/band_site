f36fa57aac0e1427473e1bc7198384c8457295c4
Zack Ryan <zackryan23@gmail.com>
2012-08-30 00:20:18 -0400

    Add validation to user registration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120822060048) do
+ActiveRecord::Schema.define(:version => 20120830040746) do
 
   create_table "profiles", :force => true do |t|
     t.text     "content"
@@ -26,5 +26,7 @@ ActiveRecord::Schema.define(:version => 20120822060048) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end

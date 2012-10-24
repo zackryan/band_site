182e34d7fd0abc2c96be6ea4ab1ad078dd012f0e
Zack Ryan <zackryan23@gmail.com>
2012-10-24 11:48:16 -0400

    Prepare for Movie 7

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 BandSite::Application.routes.draw do
 
+  resources :users
+
   root :to => 'pages#home'
 
   match '/search', to: 'pages#search'
 
   match '/signup', to: 'users#new'
 
-resources :users
+
 
 end
---
+++ spec/requests/user_pages_spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+
+describe "User pages" do
+  
+  end
+end
